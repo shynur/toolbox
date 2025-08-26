44b5d3ad1d31158f0b19486c32e6d5a4a446f80d
谢骐 <shynur@outlook.com>
2025-08-26 10:21:55 +0800

    ;

--- posix.mjs
+++ posix.mjs
@@ -6,6 +6,6 @@ export async function getconf(variable) {
     const {promisify} = await import('node:util')
     const {execFile} = await import('node:child_process')
 
-    const {stdout} = await promisify(execFile)('getconf', [variable])
+    const {stdout, stderr} = await promisify(execFile)('getconf', [variable])
     return stdout.trim()
 }
